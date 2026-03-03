8fae1778245807932a9f0362385a6cdcd1c0d267
MdRaselHosen <raselh280@gmail.com>
2026-03-03 09:38:59 +0600

    Add model via Git LFS

--- qa_app/views.py
+++ qa_app/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+import os
+import torch
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
+Model_path = "model"
+tokenizer = AutoTokenizer.from_pretrained(Model_path)
+model = AutoModelForQuestionAnswering.from_pretrained(Model_path)
+model.eval()
+torch.set_grad_enabled(False)
 
-# Create your views here.
+def predict_answer(context,question):
+    inputs = tokenizer(question,context,return_tensors="pt",truncation=True,max_length=512)
+    outputs = model(**inputs)
+
+    start_logits = outputs.start_logits
+    end_logits = outputs.end_logits
+
+    start_index = torch.argmax(start_logits)
+    end_index = torch.argmax(end_logits) + 1
+
+    answer = tokenizer.decode(
+        inputs["input_ids"][0][start_index:end_index],
+        skip_special_tokens=True
+    )
+
+    return answer
 def home(request):
-    return render(request, "home.html")
+    answer = ""
+    if request.method == "POST":
+        context = request.POST.get("context")
+        question = request.POST.get("question")
+
+        if context and question:
+            answer = predict_answer(context, question)
+
+    return render(request, "home.html", {"answer":answer})
